78251fc34d7e8da348c3b0ceee6c3393d27e57eb
trh-thanh30 <tranhuuthanhcp@gmail.com>
2025-06-09 16:36:52 +0700

    feat(app): update greeting message in AppService and configure CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('/v1/api', { exclude: ['/'] });
